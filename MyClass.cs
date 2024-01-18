7cd3591a346bfe9e30a5bcf6fbfc42c873b05a10
stacy2906 <124590794+stacy2906@users.noreply.github.com>
2024-01-18 11:06:39 +0200

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// нестатический класс
+    /// </summary>
+    class MyClass
+    {
+        /// <summary>
+        /// внутренние нестатические поля вещественного типа
+        /// </summary>
+        private double value1;
+        private double value2;
+        /// <summary>
+        /// открытое целочисленное статическое поле
+        /// </summary>
+        public static int count;
+
+        /// <summary>
+        /// нестатический конструктор с двумя параметрами,
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        public MyClass (double value1, double value2)
+        {
+            this.value1=value1;
+            this.value2 = value2;
+            
+        }
+        /// <summary>
+        /// нестатический конструктор без параметров
+        /// </summary>
+        public MyClass()
+        {
+            this.value1 = 2.0;
+            this.value2 = 2.0;
+            count++;
+        }
+
+        /// <summary>
+        ///  статический конструктор
+        /// </summary>
+        static MyClass()
+        {
+            count = 0;
+        }
+
+        /// <summary>
+        /// свойства для доступа к внутренним полям класса
+        /// </summary>
+        public double Value1
+        {
+            get { return value1; }
+            set { value1 = value; }
+        }
+
+        public double Value2
+        {
+            get { return value2; }
+            set { value2 = value; }
+        }
+        /// <summary>
+        /// метод для вывода на экран внутренних полей класса.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine("Value1: " + value1);
+            Console.WriteLine("Value2: " + value2);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MyClass myClass1 = new MyClass();
+
+            Console.WriteLine("количества созданных экземпляров: " + MyClass.count);
+
+            
+            myClass1.Print();
+            Console.ReadKey();
+        }
+    }
+}
